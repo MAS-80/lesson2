efc344da2e514a3c080e647724b50fbdcfeb941b
MAS-80 <andrej.makruschin@yandex.ru>
2024-06-13 23:21:38 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_finished=int(12)
+number_of_hours=float(1.5)
+course_name='python'
+time_for_one_task=number_of_hours/float(number_finished)
+print('Курс:',course_name,"всего задач:",number_finished,'затрачено часов:',number_of_hours,'среднее время выполнения',time_for_one_task,'часа')
